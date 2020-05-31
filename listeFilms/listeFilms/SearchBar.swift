40b94bc7dabfda28e92a60c509bd3b21576df953
Thomas JAULGEY <t.jaulgey@free.fr>
2020-05-31 21:09:11 +0200

    update searchfilter

--- listeFilms/listeFilms/SearchBar.swift
+++ listeFilms/listeFilms/SearchBar.swift
@@ -8,63 +8,37 @@
 
 import SwiftUI
 
-struct SearchBar: View {
-    @Binding var text: String
+struct SearchBar: UIViewRepresentable {
 
-    @State private var isEditing = false
-        
-    var body: some View {
-        HStack {
-            
-            TextField("Search ...", text: $text)
-                .padding(7)
-                .padding(.horizontal, 25)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 8)
-                        
-                        if isEditing {
-                            Button(action: {
-                                self.text = ""
-                                
-                            }) {
-                                Image(systemName: "multiply.circle.fill")
-                                    .foregroundColor(.gray)
-                                    .padding(.trailing, 8)
-                            }
-                        }
-                    }
-                )
-                .padding(.horizontal, 10)
-                .onTapGesture {
-                    self.isEditing = true
-                }
-            
-            if isEditing {
-                Button(action: {
-                    self.isEditing = false
-                    self.text = ""
-                    
-                    // Dismiss the keyboard
-                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                }) {
-                    Text("Cancel")
-                }
-                .padding(.trailing, 10)
-                .transition(.move(edge: .trailing))
-                .animation(.default)
-            }
-        }
-    }
-}
+     @Binding var text: String
 
-struct SearchBar_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBar(text: .constant(""))
-    }
-}
+     class Coordinator: NSObject, UISearchBarDelegate {
+
+         @Binding var text: String
+
+         init(text: Binding<String>) {
+             _text = text
+         }
+
+         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+             text = searchText
+         }
+     }
+
+     func makeCoordinator() -> SearchBar.Coordinator {
+         return Coordinator(text: $text)
+     }
+
+     func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+         let searchBar = UISearchBar(frame: .zero)
+         searchBar.delegate = context.coordinator
+         searchBar.searchBarStyle = .minimal
+        searchBar.placeholder=" Rechercher un film..."
+        searchBar.autocapitalizationType = .none
+         return searchBar
+     }
+
+     func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+         uiView.text = text
+     }
+ }
