c7256bc96540a1e4f5bd8fb10a62272964265efb
Thomas JAULGEY <t.jaulgey@free.fr>
2020-06-19 18:16:22 +0200

    modif

--- listeFilms/listeFilms/menu.swift
+++ listeFilms/listeFilms/menu.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-extension Binding where Value == Bool {
-    public func negate() -> Binding<Bool> {
-        return Binding<Bool>(get:{ !self.wrappedValue },
-            set: { self.wrappedValue = !$0})
-    }
-}
 
 struct menu: View {
     @EnvironmentObject var userData: UserData
@@ -30,7 +24,7 @@ struct menu: View {
             .padding(.vertical, 10.0)
             .foregroundColor(Color(red:0.2, green:0.6, blue:0.8))
             
-            Toggle("Uniquement les films vu", isOn: self.$showShowOnly.negate())
+            Toggle("Uniquement les films vu", isOn: self.$showShowOnly)
             .padding(.horizontal, 20.0)
             .padding(.vertical, 10.0)
             .foregroundColor(Color(red:0.2, green:0.6, blue:0.8))
@@ -55,15 +49,7 @@ struct menu: View {
                             }
                             
                         }
-                        
-//                        ForEach(0..<self.userData.movies.filter{ self.showShowOnly ? $0.isShow == !self.showShowOnly : true }.count, id: \.self) { movieIndex in
-//                            VStack (alignment: .leading) {
-//                                NavigationLink(destination: ContentView(movie: self.$userData.movies[movieIndex])) {
-//                                    Text(self.userData.movies[movieIndex].nom)
-//                                }
-//
-//                            }}
-                        
+                     
                         ForEach(self.userData.movies.filter{ self.showShowOnly ? $0.isShow == !self.showShowOnly : true }) { movie in
                             VStack (alignment: .leading) {
                                 NavigationLink(destination: ContentView(movie: movie)) {
